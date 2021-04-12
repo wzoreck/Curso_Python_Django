7bd7cf32e8da22ac599a18f2ea3c12c5812f9cc0
Daniel Wzoreck <wzoreck2000@gmail.com>
2021-04-12 14:21:11 -0300

    Exemplo de metodo estatico adicionado

--- Classes/main.py
+++ Classes/main.py
@@ -11,3 +11,10 @@ print(Pessoa.ano_atual)
 
 p2 = Pessoa.criar_pessoa_por_ano_de_nascimento('Joao', 1990)
 print(f'{p2.nome} foi criado com {p2.idade} anos, conforme o metodo de classe')
+
+# Metodo estático, não é necessário uma instância de MetodosEstaticos
+print(f'{MetodosEstaticos.mtd_estatico()} - chamado sem uma instância')
+
+# mas nada impede que o metodo estático seja chamado através de uma instância 
+mtd_est = MetodosEstaticos(10, 20)
+print(f'{mtd_est.mtd_estatico()} - chamado a partir de uma instância')
--- Classes/metodos_estaticos.py
+++ Classes/metodos_estaticos.py
@@ -4,4 +4,9 @@ class MetodosEstaticos:
         self.b = b
     
 
-    
+    # Metodos estáticos, não precisa nem de uma instância nem da classe
+    # funciona com uma função qualquer fora de classes, porem por uma questão de organização precisa estar dentro de uma classe
+    @staticmethod
+    def mtd_estatico(): # Pode receber parametros, porem não recebe referência nem da instância (self) nem da classe (exemplo cls)
+        msg = 'Este é um metodo estático'
+        return msg
