d02691cf33bb36009ccc78119203c1574df69169
Daniel Wzoreck <wzoreck2000@gmail.com>
2021-04-15 09:11:11 -0300

    Exemplo de polimorfismo adicionado

--- Polimorfismo/main.py
+++ Polimorfismo/main.py
@@ -4,3 +4,25 @@ iguais (de mesma assinatura) mas comportamentos diferentes.
 
 Mesma assinatura = Mesma quantidade e tipo de parâmetros
 """
+
+from abc import ABC, abstractmethod
+
+class A(ABC):
+    @abstractmethod
+    def fala(self, msg):
+        pass
+
+class B(A):
+    def fala(self, msg):
+        print(f'B está falando: {msg}')
+
+class C(A):
+    def fala(self, msg):
+        print(f'C está falando: {msg}')
+
+# MAIN
+b = B()
+c = C()
+
+b.fala('ABCDEFG')
+c.fala('HIJKLMN')
